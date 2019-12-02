c4ccfd1a416bd410b26f9d60522d2f1b27f8e64c
KristofferHelgesen <37105210+KristofferHelgesen@users.noreply.github.com>
2019-12-02 11:42:11 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
+import React , {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { addComment } from '@babel/types';
 
-function App() {
+const App = React.memo(props =>  {
+
+  const [redState, setRedState] = useState(240);
+  const [greenState, setGreenState] = useState(100);
+  const [blueState, setBlueState] = useState(60);
+
+  const styleC = {
+    backgroundColor : 'rgba(' + redState + ', ' + greenState + ', ' + blueState + ', 0.5)',
+    width : "200px",
+    borderRadius: "100%",
+    height: "200px",
+    margin: "auto",
+    boxShadow: '0px 0px 110px 50px rgba(' + redState + ', ' + greenState + ', ' + blueState + ', 0.5)'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+    <>
+ 
+    <div>
+      <label>Red {redState}</label>
+      <div onClick={ () =>  setRedState((redState  <= 255) ? redState-10: 255) }>-</div>
+      <div onClick={ () =>  setRedState((redState  <= 255) ? redState+10: 255 ) }>+</div>
     </div>
-  );
-}
+    <div>
+    <label>Green {greenState}</label>
+      <div onClick={ () =>  setGreenState(greenState-10) }>-</div>
+      <div onClick={ () =>  setGreenState(greenState+10) }>+</div>
+    </div>
+    <div>
+    <label>Blue {blueState} </label>
+      <div onClick={ () =>  setBlueState(blueState-10) }>-</div>
+      <div onClick={ () =>  setBlueState(blueState+10) }>+</div>
+    </div>
+
+    <div style={styleC}>
+      
+    </div>
+    </>
+  )
+});
+  
 
 export default App;
